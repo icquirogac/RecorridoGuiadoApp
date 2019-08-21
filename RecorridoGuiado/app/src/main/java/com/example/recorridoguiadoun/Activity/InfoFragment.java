d3c4ef737cd7c1325fdb569e75ae78a9ab339a9c
icquirogac <icquirogac@unal.edu.co>
2019-08-20 21:52:46 -0500

    asd

--- RecorridoGuiado/app/src/main/java/com/example/recorridoguiadoun/Activity/InfoFragment.java
+++ RecorridoGuiado/app/src/main/java/com/example/recorridoguiadoun/Activity/InfoFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -47,7 +48,11 @@ public class InfoFragment extends Fragment {
 
         Estacion estacion = GameController.saver.estacionActual;
         Resources resources = getResources();
-        imagen.setImageDrawable(resources.getDrawable(GameController.getImgSrc(estacion.nombre)));
+        try {
+            imagen.setImageDrawable(resources.getDrawable(GameController.getImgSrc(estacion.nombre)));
+        }catch (Exception x){
+
+        }
         titulo.setText(estacion.nombre);
         info.setText(estacion.info);
 
